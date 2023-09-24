dc294bb8492132c1819e79390c8f76575c33a1eb
Vishal <vishalwebdev0987@gmail.com>
2023-09-24 15:52:18 +0530

    models created

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import SchoolData, CollegeData, School, College
 # Register your models here.
+
+admin.site.register(SchoolData)
+admin.site.register(CollegeData)
+admin.site.register(School)
+admin.site.register(College)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+class College(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+
+class SchoolData(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, null=True, blank=True)
+    student = models.CharField(max_length=255, blank=True, null=True)
+    age = models.IntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=255, blank=True, null=True)
+    aadhar = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    residential_address = models.CharField(max_length=255, blank=True, null=True)
+    father_name = models.CharField(max_length=255, blank=True, null=True)
+    mother_name = models.CharField(max_length=255, blank=True, null=True)
+    father_occupation = models.CharField(max_length=255, blank=True, null=True)
+    mother_occupation = models.CharField(max_length=255, blank=True, null=True)
+    family_annual_income = models.CharField(max_length=255, blank=True, null=True)
+    contact = models.CharField(max_length=255, blank=True, null=True)
+    high_school_percentage = models.CharField(max_length=255, blank=True, null=True)
+    intermidiate_percentage = models.CharField(max_length=255, blank=True, null=True)
+
+    
+
+    
+
+    
+class CollegeData(models.Model):
+    college = models.ForeignKey(College, on_delete=models.CASCADE, null=True, blank=True)
+    student = models.CharField(max_length=255, blank=True, null=True)
+    age = models.IntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=255, blank=True, null=True)
+    aadhar = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
